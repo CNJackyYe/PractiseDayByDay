cb9dfae151379f067e57ba33d7b907ddc0419c5d
Jacky Ye <yeqw321@163.com>
2021-10-27 14:57:04 +0800

    测试提交

---
+++ 3.DrawDiamond.py
@@ -0,0 +1,29 @@
+# 画菱形 2021/1/28 难度：**
+# 题目 输入一个整数num，输出对应的菱形
+# 如输入num=3
+#   *
+#  ***
+# *****
+#  ***
+#   *
+# 如输入num=4
+#    *
+#   ***
+#  *****
+# *******
+#  *****
+#   ***
+#    *
+#
+# 能不能想出另一种解法(不使用循环)
+
+class Solution:
+    def DrawDiamond(self, num: int):
+        #在这里键入代码
+        pass
+
+
+su = Solution()
+su.DrawDiamond(3)
+su.DrawDiamond(4)
+su.DrawDiamond(10)
